a78496e4b06c6ed68a573e02a6b474cedcd690f5
DuanJiaNing <aimeimeits@gmail.com>
2020-01-06 15:31:58 +0800

    show time

--- js/blog.js
+++ js/blog.js
@@ -2,18 +2,22 @@ defaultBlogIndex = 0
 blogs = [
     {
         title: "Go 实现简单的请求路由和中间件框架",
+        time: "2020/1/6",
         path: "resource/blog/2020/1/6/Go 实现简单的请求路由和中间件框架.md"
     },
     {
         title: "JetBrains 系列 IDE 快速部署项目到远程服务器",
+        time: "2019/12/12",
         path: "resource/blog/2019/12/12/JetBrains 系列 IDE 快速部署项目到远程服务器.md"
     },
     {
         title: "Spring IOC 容器启动流程分析",
+        time: "2019/8/29",
         path: "resource/blog/2019/8/29/Spring IOC 容器启动流程分析.md"
     },
     {
         title: "go 中 for 循环对变量取地址引发的错误",
+        time: "2019/7/30",
         path: "resource/blog/2019/7/30/go 中 for 循环对变量取地址引发的错误.md"
     },
 ]
